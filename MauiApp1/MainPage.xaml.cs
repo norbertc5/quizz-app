b79ffc847326e63c4c8dfa96bd3cc8f520cd0ac1
norbertc5 <113235406+norbertc5@users.noreply.github.com>
2025-11-25 17:41:51 +0100

    added game logic

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,16 +1,112 @@
-﻿namespace MauiApp1
+﻿
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MauiApp1
 {
     public partial class MainPage : ContentPage
     {
 
+        List<TaskItem> loadedTasks = new List<TaskItem>();
+        string turn = "p1";
+        string questionText;
+        string[] answersTexts;
+        int correctAnserId;
+        int currentQuestion = 0;
+        int player1Score = 0;
+        int player2Score = 0;
+
+
         public MainPage()
         {
             InitializeComponent();
+            Loaded += MainPage_Loaded;
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private async void MainPage_Loaded(object? sender, EventArgs e)
         {
-
+            await LoadMauiAsset();
         }
+
+        async Task LoadMauiAsset()
+        {
+            using var stream = await FileSystem.OpenAppPackageFileAsync("data.json");
+            using var reader = new StreamReader(stream);
+
+            var contents = reader.ReadToEnd();
+            loadedTasks = JsonSerializer.Deserialize<List<TaskItem>>(contents) ?? loadedTasks; 
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var param = button.CommandParameter.ToString();
+
+            AnswerHandle(int.Parse(param ?? "0"));
+        }
+
+        void NextTurn()
+        {
+            if(currentQuestion >= loadedTasks.Count)
+            {
+                string winner = (player1Score > player2Score) ? "gracz pierwszy" : (player2Score > player1Score) ? "gracz drugi" : "nikt, jest remis";
+                DisplayAlert("Koniec", $"Wykorzystano wszystkie pytania. Wygrywa {winner}.", "Ok");
+                return;
+            }
+
+            questionText = loadedTasks[currentQuestion].Question;
+            answersTexts = (string[])loadedTasks[currentQuestion].Answers.Clone();
+            correctAnserId = loadedTasks[currentQuestion].CorrectAnswerId;
+            currentQuestion++;
+            DisplayAlert("info", $"{ questionText}, {answersTexts[0]}, {correctAnserId}", "OK");
+        }
+
+        void AnswerHandle(int userAnswerId)
+        {
+            string message = "";
+            if(userAnswerId == correctAnserId)
+            {
+                if(turn == "p1")
+                {
+                    message = "Poprawna odpowiedź. Podaj telefon pierwszemu graczowi.";
+                    player1Score++;
+                }
+                else
+                {
+                    message = "Poprawna odpowiedź. Podaj telefon drugiemu graczowi.";
+                    player2Score++;
+                }
+                DisplayAlert("Dobrze!", message, "OK");
+            }
+            else
+            {
+                if (turn == "p1")
+                {
+                    message = "Zła odpowiedź. Podaj telefon pierwszemu graczowi.";
+                }
+                else
+                {
+                    message = "Zła odpowiedź. Podaj telefon drugiemu graczowi.";
+                }
+                DisplayAlert("Źle!", message, "OK");
+            }
+
+            if(turn == "p1")
+            {
+                turn = "p2";
+            }
+            else
+            {
+                NextTurn();
+                turn = "p1";
+            }
+        }
+    }
+
+    class TaskItem
+    {
+        public string Question { get; set; }
+        public string[] Answers { get; set; }
+        public int CorrectAnswerId { get; set; }
     }
 }
